3fd2f919d86f4b02c2707188dd79b42cf4a88740
OliverTeo288 <oliver_teo288@hotmail.com>
2022-07-20 15:18:37 +0800

    a

--- install-prerequisites.sh
+++ install-prerequisites.sh
@@ -1,5 +1,6 @@
 echo "Starting prerequisites installation script run" && 
-sudo apt-get update && curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash - &&
+sudo apt-get update && 
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - &&
 echo "Installing node" &&
 sudo apt-get install -y nodejs &&
 sudo npm install -g pm2@latest && 
