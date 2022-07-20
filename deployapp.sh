3078ec737d65af210acfb4ae35d901d2459cf5d0
OliverTeo288 <oliver_teo288@hotmail.com>
2022-07-20 15:02:17 +0800

    Update deployapp.sh

--- deployapp.sh
+++ deployapp.sh
@@ -1,1 +1,1 @@
-echo "Starting deployment script run" && sudo npm install && sudo pm2 start -f app.js && sudo pm2 startup systemd &&  echo "End of script run"
+echo "Starting deployment script run" && sudo npm install && sudo pm2 start src/index.js && sudo pm2 startup systemd &&  echo "End of script run"
