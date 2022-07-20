f547c26ee6d0985e9432cffa10c987e0fdc6a127
OliverTeo288 <oliver_teo288@hotmail.com>
2022-07-20 14:56:29 +0800

    Update install-prerequisites.sh

--- install-prerequisites.sh
+++ install-prerequisites.sh
@@ -1,1 +1,1 @@
-echo "Starting prerequisites installation script run" && sudo apt-get update && curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - && sudo apt-get install -y nodejs && sudo ln -sf /usr/bin/nodejs /usr/bin/node && sudo npm install -g pm2 && sudo pm2 save && echo "End of script run"
+echo "Starting prerequisites installation script run" && sudo apt-get update && curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - && sudo apt-get install -y nodejs && sudo npm install -g pm2 && sudo pm2 save && echo "End of script run"
